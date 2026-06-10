aa51058f43da3e9119147db7f839c577a7419fdc
MarketingSoulPro <marketingsoul01@gmail.com>
2026-06-10 23:29:55 +0500

    Enable PWA functionality with custom install prompt and service worker

---
+++ service-worker.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'buddy-planner-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/css/base.css',
+  '/js/planner-interactive.js',
+  '/js/theme-switcher.js',
+  '/img/logo.png',
+  '/manifest.json'
+];
+
+// Install Service Worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Activate Service Worker
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Fetch Assets
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
